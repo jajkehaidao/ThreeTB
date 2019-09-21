2a24876005afafdd3f4ce624524ba09944dff012
wanggangqi <wang123@qq.com>
2019-09-21 10:07:52 +0800

    last

--- model/SqlBase.js
+++ model/SqlBase.js
@@ -8,7 +8,7 @@ class SqlBase {
       user: "root", //MySQL认证用户名
       password: "cdhq1234", //MySQL认证用户密码
       port: "10055", //端口号
-      database: "it" //数据库里面的数据
+      database: "ThreeTB" //数据库里面的数据
     });
     //2,连接
     this.connection.connect();
